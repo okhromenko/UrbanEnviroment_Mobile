648b30960e8fd1a174973a3111590dd6643d9ecc
Daria <zvereva.dv@students.dvfu.ru>
2022-03-09 00:04:41 +1000

    design registration layout

--- app/src/main/java/com/example/urbanenviroment/profile/registr_authoriz/RegistrationActivity.java
+++ app/src/main/java/com/example/urbanenviroment/profile/registr_authoriz/RegistrationActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.urbanenviroment.R;
 
@@ -19,5 +22,42 @@ public class RegistrationActivity extends AppCompatActivity {
     public void authorization(View view){
         Intent intent = new Intent(this, AuthorizationActivity.class);
         startActivity(intent);
+    }
+    public void user_register(View view){
+
+        ImageView ivname = findViewById(R.id.imageView4);
+        ivname.setImageResource(R.drawable.text_reg_user);
+
+        ImageButton ibu = findViewById(R.id.imageButton);
+        ibu.setImageResource(R.drawable.button_user_press);
+
+        ImageButton ibo = findViewById(R.id.imageButton2);
+        ibo.setImageResource(R.drawable.button_organization);
+
+        LinearLayout lldata = findViewById(R.id.linearLayoutData);
+        lldata.setVisibility(View.VISIBLE);
+
+        com.rengwuxian.materialedittext.MaterialEditText inputname = findViewById(R.id.nameField);
+        inputname.setHint("Введите имя пользователя");
+
+    }
+
+    public void org_register(View view){
+
+        ImageView ivname = findViewById(R.id.imageView4);
+        ivname.setImageResource(R.drawable.text_reg_org);
+
+        ImageButton ibu = findViewById(R.id.imageButton);
+        ibu.setImageResource(R.drawable.button_user);
+
+        ImageButton ibo = findViewById(R.id.imageButton2);
+        ibo.setImageResource(R.drawable.button_org_press);
+
+        LinearLayout lldata = findViewById(R.id.linearLayoutData);
+        lldata.setVisibility(View.VISIBLE);
+
+        com.rengwuxian.materialedittext.MaterialEditText inputname = findViewById(R.id.nameField);
+        inputname.setHint("Введите название организации");
+
     }
 }
