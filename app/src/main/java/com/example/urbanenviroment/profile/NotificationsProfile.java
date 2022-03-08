b28f566820657b5dc59306571a080a36e040c1d6
okhromenko <okhromenko.da@yandex.ru>
2022-03-08 18:52:26 +1000

    added profile

---
+++ app/src/main/java/com/example/urbanenviroment/profile/NotificationsProfile.java
@@ -0,0 +1,16 @@
+package com.example.urbanenviroment.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.urbanenviroment.R;
+
+public class NotificationsProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications_profile);
+    }
+}
