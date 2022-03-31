183ed9354e8d9322da5aef769285192cbd419579
okhromenko <okhromenko.da@yandex.ru>
2022-03-31 14:29:17 +1000

    added an animal_editing_page

--- app/src/main/java/com/example/urbanenviroment/page/profile/org/EditAnimalPage.java
+++ app/src/main/java/com/example/urbanenviroment/page/profile/org/EditAnimalPage.java
@@ -2,9 +2,16 @@ package com.example.urbanenviroment.page.profile.org;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.urbanenviroment.R;
+import com.example.urbanenviroment.page.animals.HomeActivity;
+import com.example.urbanenviroment.page.help.HelpActivity;
+import com.example.urbanenviroment.page.map.MapActivity;
+import com.example.urbanenviroment.page.org.OrganizationsActivity;
+import com.example.urbanenviroment.page.profile.registr_authoriz.AuthorizationActivity;
 
 public class EditAnimalPage extends AppCompatActivity {
 
@@ -12,5 +19,30 @@ public class EditAnimalPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_animal_page);
+    }
+
+    public void animals(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void help(View view){
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+    }
+
+    public void authorization(View view){
+        Intent intent = new Intent(this, AuthorizationActivity.class);
+        startActivity(intent);
+    }
+
+    public void organization(View view){
+        Intent intent = new Intent(this, OrganizationsActivity.class);
+        startActivity(intent);
+    }
+
+    public void map(View view){
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
