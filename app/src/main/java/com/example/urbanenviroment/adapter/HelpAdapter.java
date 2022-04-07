2e4184058848d4e210b3f892ec386da17cec3c57
Daria <zvereva.dv@students.dvfu.ru>
2022-04-08 02:13:02 +1000

    design help (help item) layout

--- app/src/main/java/com/example/urbanenviroment/adapter/HelpAdapter.java
+++ app/src/main/java/com/example/urbanenviroment/adapter/HelpAdapter.java
@@ -2,6 +2,7 @@ package com.example.urbanenviroment.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.recyclerview.widget.RecyclerView;
@@ -47,20 +49,34 @@ public class HelpAdapter extends RecyclerView.Adapter<HelpAdapter.HelpViewHolder
         holder.img_org_help.setImageResource(img_org_help_id);
 
 
-        Drawable drawable = AppCompatResources.getDrawable(context, R.drawable.help_line_item);
-        assert drawable != null;
-        Drawable wrappedDrawable = DrawableCompat.wrap(drawable);
-        Drawable mutableDrawable = wrappedDrawable.mutate();
+        //Drawable drawable = AppCompatResources.getDrawable(context, R.drawable.help_line_item);
+        //assert drawable != null;
+        //Drawable wrappedDrawable = DrawableCompat.wrap(drawable);
+        //Drawable mutableDrawable = wrappedDrawable.mutate();
 
+        int color = Color.GRAY;
+        Drawable image = ContextCompat.getDrawable(context, R.drawable.img_things_item);
 
-        if (helpList.get(position).getType_help().equals("Еда"))
-            DrawableCompat.setTint(mutableDrawable, ContextCompat.getColor(context, R.color.food));
-        if (helpList.get(position).getType_help().equals("Вещи"))
-            DrawableCompat.setTint(mutableDrawable, ContextCompat.getColor(context, R.color.things));
-        if (helpList.get(position).getType_help().equals("Волонтерство"))
-            DrawableCompat.setTint(mutableDrawable, ContextCompat.getColor(context, R.color.help));
+        if (helpList.get(position).getType_help().equals("Еда")) {
+            color = ContextCompat.getColor(context, R.color.food);
+            image = ContextCompat.getDrawable(context, R.drawable.img_food_item);
+        }
+            //DrawableCompat.setTint(mutableDrawable, ContextCompat.getColor(context, R.color.food));
+        if (helpList.get(position).getType_help().equals("Вещи")){
+            color = ContextCompat.getColor(context, R.color.things);
+            image = ContextCompat.getDrawable(context, R.drawable.img_things_item);
+        }
+            //DrawableCompat.setTint(mutableDrawable, ContextCompat.getColor(context, R.color.things));
+        if (helpList.get(position).getType_help().equals("Волонтерство")){
+            color = ContextCompat.getColor(context, R.color.help);
+            image = ContextCompat.getDrawable(context, R.drawable.img_help_item);
+        }
+            //DrawableCompat.setTint(mutableDrawable, ContextCompat.getColor(context, R.color.help));
 
-        holder.img_line_help.setImageDrawable(drawable);
+        //holder.img_line_help.setImageDrawable(drawable);
+        holder.img_item_help.setImageDrawable(image);
+        holder.img_line_help.setCardBackgroundColor(color);
+        holder.img_box_help.setCardBackgroundColor(color);
         holder.name_org_help.setText(helpList.get(position).getName_org());
         holder.type_ads_help.setText(helpList.get(position).getType_help());
         holder.description_help.setText(helpList.get(position).getDescription());
@@ -83,20 +99,22 @@ public class HelpAdapter extends RecyclerView.Adapter<HelpAdapter.HelpViewHolder
 
     public static final class HelpViewHolder extends RecyclerView.ViewHolder{
 
-        ImageView img_org_help, img_line_help;
+        CardView img_line_help, img_box_help;
+        ImageView img_org_help, img_item_help;
         TextView name_org_help, type_ads_help, description_help, status_help, date_help;
 
 
         public HelpViewHolder(@NonNull View itemView) {
             super(itemView);
-
             img_org_help = itemView.findViewById(R.id.img_org_help);
             name_org_help = itemView.findViewById(R.id.name_org_help);
             type_ads_help = itemView.findViewById(R.id.type_ads_help);
             description_help = itemView.findViewById(R.id.description_help);
             status_help = itemView.findViewById(R.id.status_help);
             date_help = itemView.findViewById(R.id.date_help);
-            img_line_help = itemView.findViewById(R.id.line_help);
+            img_line_help = itemView.findViewById(R.id.ads_line_color);
+            img_box_help = itemView.findViewById(R.id.ads_box_color);
+            img_item_help = itemView.findViewById(R.id.type_ads_img);
         }
     }
 }
