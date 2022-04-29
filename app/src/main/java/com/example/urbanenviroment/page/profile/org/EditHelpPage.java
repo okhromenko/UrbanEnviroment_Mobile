eb502619cc0cd10f64b3caa1a889b884d7c81b26
Daria <zvereva.dv@students.dvfu.ru>
2022-04-30 05:03:25 +1000

    fix: design edit_help_page layout; feat: functions for buttons on EditHelpPage

--- app/src/main/java/com/example/urbanenviroment/page/profile/org/EditHelpPage.java
+++ app/src/main/java/com/example/urbanenviroment/page/profile/org/EditHelpPage.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.urbanenviroment.R;
 import com.example.urbanenviroment.page.animals.HomeActivity;
@@ -19,6 +23,77 @@ public class EditHelpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_help_page);
+
+        // Нужно взять данные: тип объявления, дата, описание и добавить их в поля ввода\правильно окрасить кнопку.
+    }
+
+    public void change(FrameLayout frame, LinearLayout layout){
+        frame.setVisibility(View.INVISIBLE);
+        layout.setVisibility(View.VISIBLE);
+    }
+
+    public void cancel(FrameLayout frame, LinearLayout layout){
+        frame.setVisibility(View.INVISIBLE);
+        layout.setVisibility(View.GONE);
+    }
+
+    public void clear(int id){
+        TextView textName = (TextView) findViewById(id);
+        textName.setText("");
+    }
+
+    public void change_type_ads(View view){
+        FrameLayout frame = (FrameLayout) findViewById(R.id.text_change_type_ads);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_type_ads);
+
+        change(frame, layout);
+    }
+
+    public void cancel_type_ads(View view){
+        FrameLayout frame = (FrameLayout) findViewById(R.id.text_change_type_ads);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_type_ads);
+
+        ImageButton food = (ImageButton) findViewById(R.id.food_filter);
+        ImageButton things = (ImageButton) findViewById(R.id.things_filter);
+        ImageButton help = (ImageButton) findViewById(R.id.help_filter);
+
+        //Нужно сделать определение типа и окрасить только ту кнопку, какого типа объявление нажато.
+
+        food.setImageResource(R.drawable.button_food_type_ad_org);
+        things.setImageResource(R.drawable.button_things_type_ad_org);
+        help.setImageResource(R.drawable.button_help_type_ad_org);
+
+        cancel(frame, layout);
+    }
+
+    public void change_last_date(View view){
+        FrameLayout frame = (FrameLayout) findViewById(R.id.text_change_last_date);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_last_date);
+
+        change(frame, layout);
+    }
+
+    public void cancel_last_date(View view){
+        FrameLayout frame = (FrameLayout) findViewById(R.id.text_change_last_date);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_last_date);
+
+        cancel(frame, layout);
+        clear(R.id.text_edit_last_date);
+    }
+
+    public void change_description(View view){
+        FrameLayout frame = (FrameLayout) findViewById(R.id.text_change_description);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_description);
+
+        change(frame, layout);
+    }
+
+    public void cancel_description(View view){
+        FrameLayout frame = (FrameLayout) findViewById(R.id.text_change_description);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_description);
+
+        cancel(frame, layout);
+        clear(R.id.text_edit_description);
     }
 
     public void animals(View view){
