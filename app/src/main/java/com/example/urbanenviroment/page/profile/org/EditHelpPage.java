ad507908a08e06c7d68a8881c823882b54169957
Daria <zvereva.dv@students.dvfu.ru>
2022-06-11 21:15:46 +1000

    fix: add onclick on textview(first_date) on add ads, hide notification icon on org item; feat: add change first_date on edit ads page;

--- app/src/main/java/com/example/urbanenviroment/page/profile/org/EditHelpPage.java
+++ app/src/main/java/com/example/urbanenviroment/page/profile/org/EditHelpPage.java
@@ -26,6 +26,7 @@ import com.example.urbanenviroment.page.help.HelpActivity;
 import com.example.urbanenviroment.page.org.OrganizationsActivity;
 import com.example.urbanenviroment.page.profile.registr_authoriz.AuthorizationActivity;
 import com.example.urbanenviroment.page.profile.settings.SettingPageOrg;
+import com.example.urbanenviroment.page.profile.settings.SettingProfileUser;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -40,7 +41,11 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 public class EditHelpPage extends AppCompatActivity {
@@ -48,7 +53,7 @@ public class EditHelpPage extends AppCompatActivity {
     private FirebaseFirestore db;
     int type_flag = 0;
     String type, id, description;
-    MaterialEditText text_edit_last_date;
+    MaterialEditText text_edit_last_date, text_edit_first_date;
     Calendar calendar_text;
     DatePickerDialog dpd;
 
@@ -59,6 +64,7 @@ public class EditHelpPage extends AppCompatActivity {
 
         TextView type_ads = findViewById(R.id.text_type_ads_edit_page);
         TextView date_ads = findViewById(R.id.text_data_ads_edit_page);
+        TextView first_date_ads = findViewById(R.id.text_first_data_ads_edit_page);
         EditText description_ads = findViewById(R.id.text_edit_help_description);
 
         db = FirebaseFirestore.getInstance();
@@ -76,7 +82,27 @@ public class EditHelpPage extends AppCompatActivity {
                             description = document.getString("description");
                             type_ads.setText(document.getString("type"));
                             date_ads.setText(document.getString("last_date"));
+                            first_date_ads.setText(document.getString("first_date"));
                             description_ads.setText(description);
+
+                            LinearLayout first_date_ll = (LinearLayout) findViewById(R.id.layout_change_first_date);
+                            FrameLayout first_date_fl = (FrameLayout) findViewById(R.id.text_change_first_date);
+                            TextView first_date_tv = (TextView) findViewById(R.id.text_no_first_date);
+
+                            @SuppressLint("SimpleDateFormat") SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+                            Date date_first = new Date();
+                            Date date_last = null;
+                            try {
+                                date_last = format.parse(document.getString("first_date"));
+                                assert date_first != null;
+                                if (date_first.compareTo(date_last) >= 0) {
+                                    first_date_ll.setVisibility(View.GONE);
+                                    first_date_fl.setVisibility(View.GONE);
+                                    first_date_tv.setVisibility(View.VISIBLE);
+                                }
+                            } catch (java.text.ParseException e) {
+                                e.printStackTrace();
+                            }
                         } else {
                             Log.d(TAG, "Данные не найдены");
                         }
@@ -86,11 +112,11 @@ public class EditHelpPage extends AppCompatActivity {
         }
         else{
             type_ads.setText(getIntent().getStringExtra("type_ads_help"));
-            date_ads.setText(getIntent().getStringExtra("date_help"));
+            date_ads.setText(getIntent().getStringExtra("date_first_help"));
+            first_date_ads.setText(getIntent().getStringExtra("date_help"));
             description_ads.setText(getIntent().getStringExtra("description_help"));
             description = getIntent().getStringExtra("description_help");
         }
-
     }
 
     public void change(FrameLayout frame, LinearLayout layout){
@@ -145,6 +171,21 @@ public class EditHelpPage extends AppCompatActivity {
         clear(R.id.text_edit_last_date);
     }
 
+    public void change_first_date(View view){
+        FrameLayout frame = (FrameLayout) findViewById(R.id.text_change_first_date);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_first_date);
+
+        change(frame, layout);
+    }
+
+    public void cancel_first_date(View view){
+        FrameLayout frame = (FrameLayout) findViewById(R.id.text_change_first_date);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_first_date);
+
+        cancel(frame, layout);
+        clear(R.id.text_edit_first_date);
+    }
+
     public void cancel_description(View view){
         LinearLayout layout = (LinearLayout) findViewById(R.id.layout_change_description);
         EditText desc = (EditText) findViewById(R.id.text_edit_help_description);
@@ -164,6 +205,33 @@ public class EditHelpPage extends AppCompatActivity {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 text_edit_last_date.setText(dayOfMonth + "." + (month + 1) + "." + year);
+            }
+        }, year_first, month_first, day_first);
+        dpd.show();
+    }
+
+    public void calendar_first(View view){
+        text_edit_first_date = (MaterialEditText) findViewById(R.id.text_edit_first_date);
+        calendar_text = Calendar.getInstance();
+
+        int day_first = calendar_text.get(Calendar.DAY_OF_MONTH);
+        int month_first = calendar_text.get(Calendar.MONTH);
+        int year_first = calendar_text.get(Calendar.YEAR);
+
+        dpd = new DatePickerDialog(EditHelpPage.this, new DatePickerDialog.OnDateSetListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String Month, Day;
+                Month = Integer.toString(month + 1);
+                if (Month.length() == 1) {
+                    Month = "0" + Month;
+                }
+                Day = Integer.toString(dayOfMonth);
+                if (Day.length() == 1) {
+                    Day = "0" + Day;
+                }
+                text_edit_first_date.setText(Day + "." + Month + "." + year);
             }
         }, year_first, month_first, day_first);
         dpd.show();
@@ -211,7 +279,6 @@ public class EditHelpPage extends AppCompatActivity {
             @Override
             public void onSuccess(Void unused) {
                 Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_LONG).show();
-
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -241,12 +308,15 @@ public class EditHelpPage extends AppCompatActivity {
                 type = "Волонтерство";
                 break;
         }
-
         save("type", type);
     }
 
     public void save_last_date(View view){
         save("last_date", Objects.requireNonNull(text_edit_last_date.getText()).toString());
+    }
+
+    public void save_first_date(View view){
+        save("first_date", Objects.requireNonNull(text_edit_first_date.getText()).toString());
     }
 
     public void save_description(View view){
